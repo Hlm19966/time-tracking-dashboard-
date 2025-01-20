9b46424596c69b1002aeb5f90e3301a575eee16d
Heli135 <164352976+Hlm19966@users.noreply.github.com>
2025-01-20 11:44:17 +0330

    added js and css

---
+++ main.js
@@ -0,0 +1,35 @@
+let globalData = [];
+
+fetch('./data.json').then(response => {
+    if (!response.ok) {
+    throw new Error ('Erorr fetching JSON');
+    }
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+    globalData = data;
+})
+.catch(error => {
+    console.error('Erorr:', error);
+});
+
+
+function displayData(data, timeframes) {
+    const hours = document.querySelectorAll(".hours");
+    const previousHours = document.querySelectorAll(".previous-hours");
+    data.forEach((item, index) => {
+    console.log(item.timeframes.daily.current);
+    console.log(item.timeframes.weekly.current);
+
+    hours[index].innerHTML = `${item.timeframes[timeframes].current}hrs`;
+    previousHours[index].innerHTML = item.timeframes[timeframes].previous;
+});
+  
+}
+
+document.getElementById("daily").addEventListener("click", () => displayData(globalData, 'daily'));
+document.getElementById("weekly").addEventListener("click", () => displayData(globalData, 'weekly'));
+document.getElementById("monthly").addEventListener("click", () => displayData(globalData, 'monthly'));
+
+
